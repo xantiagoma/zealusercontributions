91f4912f965768b0a74dc38e5fb5d7086ed29a06
Jasper van Merle <jaspervmerle@gmail.com>
2020-08-28 23:33:33 +0200

    Revert Content-Type header change on /api/docsets endpoint

--- pages/api/docsets.js
+++ pages/api/docsets.js
@@ -1,5 +1,4 @@
 import { getDocsets } from "../../src/utils";
 export default (req, res) => {
-  res.setHeader("Content-Type", "application/xml");
   getDocsets().then((d) => res.status(200).json(d));
 };
