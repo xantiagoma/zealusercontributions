208cf4c976be2d0acf4f006f8f2e8bab9bc8fbda
Santiago Montoya <xantiagoma@gmail.com>
2020-09-08 22:54:29 -0500

    Mobile view, move styles to scss

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,15 @@ const Home = ({ repos }) => (
     </Head>
 
     <main>
+      <img src="/favicon-192.png" alt="Zeal Logo" className="logo" /> <br />
       <h1 className="title">
-        <img src="/favicon-192.png" alt="Zeal Logo" /> <br />
         Welcome to
         <br />
         Zeal User Contributions
       </h1>
-
       <p className="description">
         Non-Official Zeal User Contributions Repository
       </p>
-
       <div className="grid">
         {repos.map(
           ({
@@ -103,148 +101,6 @@ const Home = ({ repos }) => (
         <code>xantiagoma</code>
       </a>
     </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        flex-wrap: wrap;
-        margin-top: 3rem;
-        max-width: 90vw;
-      }
-
-      .card {
-        margin: 1rem auto;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-        flex-basis: 47%;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        border-color: #0070f3;
-      }
-
-      a:hover,
-      a:focus {
-        color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
   </div>
 );
 
